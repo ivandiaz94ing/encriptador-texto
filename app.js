2d3a1b0c7c43bc185eddf8f449f374aa1eb6d4c6
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-09-13 05:47:06 -0500

    funcion copiar texto en portapapeles y alerta

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let bandera = false;
+let contenidoVacio = document.getElementById('contenido-vacio');
+let textoResultado = document.querySelector('.texto-resultado'); 
+let contenedorPadre = document.getElementById('contenedor-padre');
+
+
 
 function desencriptar(texto){
     let textoDesencriptado = texto
@@ -11,10 +17,28 @@ function desencriptar(texto){
 
 function retornar (){
     let texto = document.getElementById('textArea').value;
-    console.log(`El texto normal es: ${texto} y como resultado es : ${encriptar(texto)}` );
+    if (texto.length > 1) {
+        contenidoVacio.style.display = 'none';
+        textoResultado.style.display = 'flex'
+        textoResultado.textContent = encriptar(texto);
+    }else{
+            contenidoVacio.style.display = 'flex';
+            textoResultado.style.display = 'none'
+            
+    }
 }
 function retornar1 (){
     let texto = document.getElementById('textArea').value;
+    if (texto.length > 1) {
+        textoResultado.textContent = desencriptar(texto);
+        contenidoVacio.style.display = 'none';
+        textoResultado.style.display = 'flex'
+
+    }else{
+        contenidoVacio.style.display = 'flex';
+        textoResultado.style.display = 'none'
+
+    }
     console.log(desencriptar(texto));
 }
 
@@ -28,3 +52,21 @@ function encriptar(texto){
     return textoEncriptado;
     }
     
+
+    function copiarTexto(){
+        let texto = document.getElementById('texto-a-copiar').textContent;
+        navigator.clipboard.writeText(texto).then(function(){
+            Swal.fire({
+                title: 'Notificación!',
+                text: 'Texto copiado en el portapapeles',
+                icon: 'success',
+                timer: 3000,  // Se cierra automáticamente después de 3 segundos
+                showConfirmButton: false
+              });
+       
+        }).catch(function(err){
+            console.error( 'Error al copiar texto ', err);
+        });
+    }
+
+  
